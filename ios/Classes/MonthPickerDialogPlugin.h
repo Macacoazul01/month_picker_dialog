465271af3a7159ee632f147455317fb484bd0136
hmkrivoj <krivoj@hm.edu>
2018-12-08 21:30:10 +0100

    Converted to package (used to be a plugin)

--- ios/Classes/MonthPickerDialogPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface MonthPickerDialogPlugin : NSObject<FlutterPlugin>
-@end
--- ios/Classes/MonthPickerDialogPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "MonthPickerDialogPlugin.h"
-#import <month_picker_dialog/month_picker_dialog-Swift.h>
-
-@implementation MonthPickerDialogPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftMonthPickerDialogPlugin registerWithRegistrar:registrar];
-}
-@end
