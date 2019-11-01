2ec0dc22cd38ce7e63f693a38e49561fdecad88d
Sagar Zala <sagarzala123@users.noreply.github.com>
2019-11-01 10:09:04 +0530

    Update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -64,11 +64,13 @@ class _MyAppState extends State<MyApp> {
                   lastDate: DateTime( DateTime.now().year + 1, 9 ),
                   initialDate: selectedDate ?? widget.initialDate
               )
-                  .then((date) => setState(() {
-                  if (date != null) {
-                    selectedDate = date;
-                  }
-              }));
+                  .then((date) => () {
+                        if (date != null) {
+                          setState(() {
+                            selectedDate = date;
+                          });
+                        }
+                  });
             },
             child: Icon(Icons.calendar_today),
           ),
