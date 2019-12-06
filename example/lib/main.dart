6439325b0807f0ea414e5bc1454a6da71ef215b3
hmkrivoj <krivoj@hm.edu>
2019-12-06 12:14:22 +0100

    :bug: fixed bug in example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,27 +3,27 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:month_picker_dialog/month_picker_dialog.dart';
 
 void main() => runApp(MyApp(
-  initialDate: DateTime.now(),
-));
+      initialDate: DateTime.now(),
+    ));
 
 class MyApp extends StatefulWidget {
   final DateTime initialDate;
-  
+
   const MyApp({Key key, @required this.initialDate}) : super(key: key);
-  
+
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
   DateTime selectedDate;
-  
+
   @override
   void initState() {
     super.initState();
     selectedDate = widget.initialDate;
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -59,17 +59,16 @@ class _MyAppState extends State<MyApp> {
           builder: (context) => FloatingActionButton(
             onPressed: () {
               showMonthPicker(
-                  context: context,
-                  firstDate: DateTime( DateTime.now().year - 1 , 5),
-                  lastDate: DateTime( DateTime.now().year + 1, 9 ),
-                  initialDate: selectedDate ?? widget.initialDate
-              )
-                  .then((date) => () {
-                        if (date != null) {
-                          setState(() {
-                            selectedDate = date;
-                          });
-                        }
+                      context: context,
+                      firstDate: DateTime(DateTime.now().year - 1, 5),
+                      lastDate: DateTime(DateTime.now().year + 1, 9),
+                      initialDate: selectedDate ?? widget.initialDate)
+                  .then((date) {
+                    if (date != null) {
+                      setState(() {
+                        selectedDate = date;
+                      });
+                    }
                   });
             },
             child: Icon(Icons.calendar_today),
