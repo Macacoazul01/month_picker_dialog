b4c4cd413c731010b27413f0fe11932db86ade38
Serhii Sinelnikov <sinelnikov.serhii@gmail.com>
2024-07-04 13:56:46 +0200

    chore: add scriptCode option to locale

--- lib/src/helpers/locale_utils.dart
+++ lib/src/helpers/locale_utils.dart
@@ -5,9 +5,27 @@ String getLocale(
   BuildContext context, {
   Locale? selectedLocale,
 }) {
-  if (selectedLocale != null) {
-    return '${selectedLocale.languageCode}_${selectedLocale.countryCode}';
+  // Function to create the locale string, including scriptCode if available
+  String createLocaleString(Locale locale) {
+    final languageCode = locale.languageCode;
+    final scriptCode = locale.scriptCode ?? ''; // Default to empty if scriptCode is null
+    final countryCode = locale.countryCode ?? ''; // Default to empty if countryCode is null
+
+    if (scriptCode.isNotEmpty && countryCode.isNotEmpty) {
+      return '${languageCode}_${scriptCode}_${countryCode}';
+    } else if (scriptCode.isNotEmpty) {
+      return '${languageCode}_${scriptCode}';
+    } else if (countryCode.isNotEmpty) {
+      return '${languageCode}_${countryCode}';
+    } else {
+      return languageCode;
+    }
   }
+
+  if (selectedLocale != null) {
+    return createLocaleString(selectedLocale);
+  }
+
   final Locale locale = Localizations.localeOf(context);
-  return '${locale.languageCode}_${locale.countryCode}';
+  return createLocaleString(locale);
 }
