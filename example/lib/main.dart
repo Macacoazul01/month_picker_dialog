9d730f2482c18d2049ce325402203816929be73d
Gianluca Bettega <gian_bettega@hotmail.com>
2024-07-28 14:57:21 -0300

    fixed sample

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,8 +3,41 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:month_picker_dialog/month_picker_dialog.dart';
 
 void main() => runApp(
-      MyApp(
-        initialDate: DateTime.now(),
+      MaterialApp(
+        localizationsDelegates: const <LocalizationsDelegate<dynamic>>[
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: const <Locale>[
+          Locale('en'),
+          Locale('zh'),
+          Locale('fr'),
+          Locale('es'),
+          Locale('de'),
+          Locale('ru'),
+          Locale('ja'),
+          Locale('ar'),
+          Locale('fa'),
+          Locale('es'),
+          Locale('it'),
+        ],
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.indigo)
+              .copyWith(secondary: Colors.pinkAccent),
+          //Changing the global dialog border
+          dialogTheme: const DialogTheme(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(20),
+              ),
+              side: BorderSide(color: Colors.red),
+            ),
+          ),
+        ),
+        home: MyApp(
+          initialDate: DateTime.now(),
+        ),
       ),
     );
 
@@ -27,138 +60,57 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    // return CupertinoApp(
-    return MaterialApp(
-      localizationsDelegates: const <LocalizationsDelegate<dynamic>>[
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: const <Locale>[
-        Locale('en'),
-        Locale('zh'),
-        Locale('fr'),
-        Locale('es'),
-        Locale('de'),
-        Locale('ru'),
-        Locale('ja'),
-        Locale('ar'),
-        Locale('fa'),
-        Locale('es'),
-        Locale('it'),
-      ],
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.indigo)
-            .copyWith(secondary: Colors.pinkAccent),
-        //Changing the global dialog border
-        dialogTheme: const DialogTheme(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(
-              Radius.circular(20),
-            ),
-            side: BorderSide(color: Colors.red),
-          ),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Month Picker Example App'),
       ),
-      // theme: CupertinoThemeData(primaryColor: Colors.indigo),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Month Picker Example App'),
-        ),
-        body: Center(
-          child: Text(
-            'Year: ${selectedDate?.year}\nMonth: ${selectedDate?.month}',
-            style: Theme.of(context).textTheme.headlineMedium,
-            textAlign: TextAlign.center,
-          ),
-        ),
-        floatingActionButton: Builder(
-          builder: (BuildContext context) => FloatingActionButton(
-            onPressed: () {
-              /* showMonthPicker(
-                context: context,
-                firstDate: DateTime(DateTime.now().year - 5, 5),
-                lastDate: DateTime(DateTime.now().year + 8, 9),
-                initialDate: selectedDate ?? widget.initialDate,
-                locale: const Locale('en'),
-                //show only even months
-                selectableMonthPredicate: (DateTime val) => val.month.isEven,
-                monthStylePredicate: (DateTime val) {
-                  if (val.month == 4) {
-                    return TextButton.styleFrom(
-                      backgroundColor: Colors.yellow[700],
-                      textStyle: const TextStyle(
-                        color: Colors.pink,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    );
-                  }
-                  return null;
-                },
-                yearStylePredicate: (int val) {
-                  if (val == 2022) {
-                    return TextButton.styleFrom(
-                      backgroundColor: Colors.yellow[700],
-                      textStyle: const TextStyle(
-                        color: Colors.pink,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    );
-                  }
-                  return null;
-                },
-                headerColor: Colors.indigo[300],
-                headerTextColor: Colors.black,
-                selectedMonthBackgroundColor: Colors.amber[900],
-                selectedMonthTextColor: Colors.white,
-                unselectedMonthTextColor: Colors.black,
-                currentMonthTextColor: Colors.green,
-                confirmWidget: Text(
-                  'This one!',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.indigo[300],
-                  ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 30),
+          child: Center(
+            child: Column(
+              children: [
+                Text(
+                  'Year: ${selectedDate?.year}\nMonth: ${selectedDate?.month}',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                  textAlign: TextAlign.center,
                 ),
-                cancelWidget: Text(
-                  'Cancel',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.red[900],
-                  ),
+                const SizedBox(
+                  height: 20,
                 ),
-                roundedCornersRadius: 20,
-                yearFirst: false,
-                backgroundColor: Colors.blueGrey[50],
-                buttonBorder: const RoundedRectangleBorder(),
-                headerTitle: const Text('Month Picker Dialog'),
-                // customDivider: Divider(
-                //   color: Colors.black,
-                //   endIndent: 25,
-                //   indent: 25,
-                // ),
-                // forcePortrait: true,
-                //forceSelectedDate: true,
-                //dismissible: true,
-                // capitalizeFirstLetter: true,
-                // customHeight: 500,
-                // customWidth: 500,
-                // dismissible: true,
-                // forceSelectedDate: true,
-                // animationMilliseconds: 300
-                //hideHeaderRow: true
-              ).then((DateTime? date) {
-                if (date != null) {
-                  setState(() {
-                    selectedDate = date;
-                  });
-                }
-              }); */
-              showMonthRangePicker(
+                TextButton(
+                  onPressed: () async {
+                    await showMonthPicker(
                       context: context,
                       firstDate: DateTime(DateTime.now().year - 5, 5),
                       lastDate: DateTime(DateTime.now().year + 8, 9),
                       initialDate: selectedDate ?? widget.initialDate,
+                      selectableMonthPredicate: (DateTime val) =>
+                          val.month.isEven,
+                      monthStylePredicate: (DateTime val) {
+                        if (val.month == 4) {
+                          return TextButton.styleFrom(
+                            backgroundColor: Colors.yellow[700],
+                            textStyle: const TextStyle(
+                              color: Colors.pink,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          );
+                        }
+                        return null;
+                      },
+                      yearStylePredicate: (int val) {
+                        if (val == 2022) {
+                          return TextButton.styleFrom(
+                            backgroundColor: Colors.yellow[700],
+                            textStyle: const TextStyle(
+                              color: Colors.pink,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          );
+                        }
+                        return null;
+                      },
                       confirmWidget: Text(
                         'This one!',
                         style: TextStyle(
@@ -173,66 +125,87 @@ class _MyAppState extends State<MyApp> {
                           color: Colors.red[900],
                         ),
                       ),
-                      headerTitle: const Text('Month Picker Dialog'),
-                      monthPickerDialogSettings: MonthPickerDialogSettings(
-                        dialogSettings: PickerDialogSettings(
-                          locale: const Locale('en'),
-                          dialogRoundedCornersRadius: 20,
-                          dialogBackgroundColor: Colors.blueGrey[50],
-                        ),
-                        headerSettings: PickerHeaderSettings(
-                          headerBackgroundColor: Colors.indigo[300],
-                          headerSelectedIntervalTextStyle: const TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                          ),
-                          headerPageTextStyle: const TextStyle(
-                            color: Colors.black,
-                          ),
-                          previousIcon: Icons.arrow_back,
-                          nextIcon: Icons.arrow_forward,
-                        ),
-                        buttonsSettings: PickerButtonsSettings(
-                          buttonBorder: const RoundedRectangleBorder(),
-                          selectedMonthBackgroundColor: Colors.amber[900],
-                          selectedMonthTextColor: Colors.white,
-                          unselectedMonthsTextColor: Colors.black,
-                          currentMonthTextColor: Colors.green,
-                          yearTextStyle: const TextStyle(
-                            color: Colors.amber,
-                          ),
-                          monthTextStyle: const TextStyle(
-                            color: Colors.red,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                      // customDivider: Divider(
-                      //   color: Colors.black,
-                      //   endIndent: 25,
-                      //   indent: 25,
-                      // ),
-                      // forcePortrait: true,
-                      //forceSelectedDate: true,
-                      //dismissible: true,
-                      // capitalizeFirstLetter: true,
-                      // customHeight: 500,
-                      // customWidth: 500,
-                      // dismissible: true,
-                      // forceSelectedDate: true,
-                      // animationMilliseconds: 300
-                      //hideHeaderRow: true
-                      rangeList: true)
-                  .then((List<DateTime>? dates) {
-                if (dates != null) {
-                  print(dates);
-                  print(dates.last.lastDayOfMonth());
-                }
-              });
-            },
-            child: const Icon(Icons.calendar_today),
+                    ).then((DateTime? date) {
+                      if (date != null) {
+                        setState(() {
+                          selectedDate = date;
+                        });
+                      }
+                    });
+                  },
+                  child: const Text('Single month picker'),
+                )
+              ],
+            ),
           ),
+        ),
+      ),
+      floatingActionButton: Builder(
+        builder: (BuildContext context) => FloatingActionButton(
+          onPressed: () {
+            showMonthRangePicker(
+              context: context,
+              firstDate: DateTime(DateTime.now().year - 5, 5),
+              lastDate: DateTime(DateTime.now().year + 8, 9),
+              initialDate: selectedDate ?? widget.initialDate,
+              confirmWidget: Text(
+                'This one!',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.indigo[300],
+                ),
+              ),
+              cancelWidget: Text(
+                'Cancel',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red[900],
+                ),
+              ),
+              headerTitle: const Text('Month Picker Dialog'),
+              monthPickerDialogSettings: MonthPickerDialogSettings(
+                dialogSettings: PickerDialogSettings(
+                  locale: const Locale('en'),
+                  dialogRoundedCornersRadius: 20,
+                  dialogBackgroundColor: Colors.blueGrey[50],
+                ),
+                headerSettings: PickerHeaderSettings(
+                  headerBackgroundColor: Colors.indigo[300],
+                  headerSelectedIntervalTextStyle: const TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                  headerPageTextStyle: const TextStyle(
+                    color: Colors.black,
+                  ),
+                  previousIcon: Icons.arrow_back,
+                  nextIcon: Icons.arrow_forward,
+                ),
+                buttonsSettings: PickerButtonsSettings(
+                  buttonBorder: const RoundedRectangleBorder(),
+                  selectedMonthBackgroundColor: Colors.amber[900],
+                  selectedMonthTextColor: Colors.white,
+                  unselectedMonthsTextColor: Colors.black,
+                  currentMonthTextColor: Colors.green,
+                  yearTextStyle: const TextStyle(
+                    color: Colors.amber,
+                  ),
+                  monthTextStyle: const TextStyle(
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              rangeList: true,
+            ).then((List<DateTime>? dates) {
+              if (dates != null) {
+                print(dates);
+                print(dates.last.lastDayOfMonth());
+              }
+            });
+          },
+          child: const Icon(Icons.calendar_today),
         ),
       ),
     );
