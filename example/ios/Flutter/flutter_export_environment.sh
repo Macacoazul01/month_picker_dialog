29f3a451dedf0ad673fe0313831a96c7d318c1f7
Macacoazul01 <gian_bettega@hotmail.com>
2025-02-23 10:36:34 -0300

    ios files

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\gian_\Desktop\month_picker_dialog\example"
+export "FLUTTER_ROOT=/Users/gian/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/gian/Desktop/month_picker_dialog/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=/Users/gian/Desktop/month_picker_dialog/example/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/gian/Desktop/month_picker_dialog/example/.dart_tool/package_config.json"
